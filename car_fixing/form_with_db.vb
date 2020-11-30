6e5e7a7b65f8ddfe03583d1f368fb3a1f9d59183
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-11-30 22:56:51 +0200

    fixed naming

--- car_fixing/form_with_db.vb
+++ car_fixing/form_with_db.vb
@@ -53,9 +53,7 @@
     End Sub
 
     Private Sub form_with_db_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'Car_brand_dataset.Table' table. You can move, or remove it, as needed.
         Me.TableTableAdapter1.Fill(Me.Car_brand_dataset.Table)
-        'TODO: This line of code loads data into the 'Car_databaseDataSet.Table' table. You can move, or remove it, as needed.
         Me.TableTableAdapter.Fill(Me.Car_databaseDataSet.Table)
         Datedtp.Visible = False
         rateus.Visible = False
--- car_fixing/view_database.vb
+++ car_fixing/view_database.vb
@@ -1,6 +1,5 @@
 ﻿Public Class view_database
     Private Sub view_database_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'Car_databaseDataSet.Table' table. You can move, or remove it, as needed.
         Me.TableTableAdapter.Fill(Me.Car_databaseDataSet.Table)
 
     End Sub
